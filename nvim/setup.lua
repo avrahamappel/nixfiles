503496f847162f95c35593f44864e8644cdb5e42
Avraham Appel <aappel248@gmail.com>
2023-02-06 08:16:29 -0500

    Don't show diagnostics in .env files
    .env files are considered bash files by polyglot, and the bash language
    server jumps in and invokes shellcheck which gives a gazillion errors
    about unused variables.

--- nvim/setup.lua
+++ nvim/setup.lua
@@ -169,3 +169,13 @@ cmp.setup({
 -- require 'lsp_signature'.setup({
 --     hint_prefix = "", -- no panda
 -- })
+
+-- Disable diagnostics in .env files
+local group = vim.api.nvim_create_augroup("__env", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = ".env*",
+    group = group,
+    callback = function(args)
+        vim.diagnostic.disable(args.buf)
+    end
+})
