9809476b4ac420b231639a1d414b1a28dcba0cce
Avraham Appel <aappel248@gmail.com>
2023-04-26 17:35:30 -0400

    Disable NeoVim providers

--- nvim/default.nix
+++ nvim/default.nix
@@ -5,6 +5,11 @@
 
   defaultEditor = true;
 
+  # Don't use any providers for now.
+  withNodeJs = false;
+  withPython3 = false;
+  withRuby = false;
+
   # List of vim plugins to install optionally associated with configuration to be placed in init.vim.
   plugins = with pkgs.vimPlugins; [
     # Nothing wrong with some extra speed
