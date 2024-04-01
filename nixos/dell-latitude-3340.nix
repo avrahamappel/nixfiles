26a36e3d2c23f16771a52ba060ede1e1fa169fa5
Avraham Appel <aappel248@gmail.com>
2024-04-01 01:02:08 -0400

    Memory performance

--- nixos/dell-latitude-3340.nix
+++ nixos/dell-latitude-3340.nix
@@ -7,4 +7,13 @@
       patch = ./keyboard-after-suspend-fix.patch;
     }
   ];
+
+  # Some memory tweaks to improve performance hopefully
+  boot.kernel.sysctl = {
+    "vm.dirty_ratio" = 1;
+    "vm.dirty_background_ratio" = 2;
+    "vm.overcommit_memory" = 2;
+  };
+  # Make browsers faster
+  services.psd.enable = true;
 }
