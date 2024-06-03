eb0bc8a5a6633cdcd45a9397093bb020610f023e
Avraham Appel <aappel248@gmail.com>
2024-06-03 19:51:26 -0400

    NixOS 24.05

--- home.nix
+++ home.nix
@@ -10,7 +10,7 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "23.11";
+    stateVersion = "24.05";
     enableNixpkgsReleaseCheck = true;
 
     # TODO move aliases here
