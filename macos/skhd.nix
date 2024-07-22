4b5bf9e0e138f05db27fef51fa6f39e40cc84f6f
Avraham Appel <aappel248@gmail.com>
2024-07-22 15:54:16 -0400

    Allow other modules to add skhd shortcuts

--- macos/skhd.nix
+++ macos/skhd.nix
@@ -1,29 +1,46 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
+
+let
+  cfg = config.programs.skhd;
+in
 
 {
-  home.packages = with pkgs; [
-    skhd
-  ];
+  options.programs.skhd = {
+    extraConfig = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      description = "Extra skhd configuration lines";
+    };
+  };
 
-  home.file.".config/skhd/skhdrc" = {
-    text = ''
-      # Open alacritty on cmd - return
-      # if there's already an instance running, open new window, otherwise start a new instance
-      ctrl + cmd - return : ${config.programs.alacritty.package}/bin/alacritty msg create-window 2>&1 || /usr/bin/open -a ${config.programs.alacritty.package}/Applications/Alacritty.app
-    '';
 
-    onChange = ''
-      # Nix version doesn't seem to pick up changes
-      if [[ -f "/tmp/skhd_$USER.pid" ]]; then
-        ${pkgs.skhd}/bin/skhd --reload
+  config = {
+    home.packages = with pkgs; [
+      skhd
+    ];
+
+    home.file.".config/skhd/skhdrc" = {
+      text = ''
+        # Open alacritty on cmd - return
+        # if there's already an instance running, open new window, otherwise start a new instance
+        ctrl + cmd - return : ${config.programs.alacritty.package}/bin/alacritty msg create-window 2>&1 || /usr/bin/open -a ${config.programs.alacritty.package}/Applications/Alacritty.app
+
+        ${cfg.extraConfig}
+      '';
+
+      onChange = ''
+        # Nix version doesn't seem to pick up changes
+        if [[ -f "/tmp/skhd_$USER.pid" ]]; then
+          ${pkgs.skhd}/bin/skhd --reload
+        fi
+      '';
+    };
+
+    home.activation.install-skhd = ''
+      if [[ ! -f "$HOME/Library/LaunchAgents/com.koekeishiya.skhd.plist" ]]; then
+        echo "Don't forget to install the skhd service!"
+        echo '`skhd --install-service`'
       fi
     '';
   };
-
-  home.activation.install-skhd = ''
-    if [[ ! -f "$HOME/Library/LaunchAgents/com.koekeishiya.skhd.plist" ]]; then
-      echo "Don't forget to install the skhd service!"
-      echo '`skhd --install-service`'
-    fi
-  '';
 }
