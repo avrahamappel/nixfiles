4ac91aeb433c72aa22c3bf416ee2a189a01867d8
Avraham Appel <aappel248@gmail.com>
2024-07-29 22:06:46 -0400

    Hot apps option

--- macos/skhd.nix
+++ macos/skhd.nix
@@ -2,17 +2,27 @@
 
 let
   cfg = config.programs.skhd;
+
+  hotApps = lib.strings.concatLines (lib.lists.imap1
+    (idx: app: ''
+      cmd - ${builtins.toString (idx + 1)} : open -a ${builtins.replaceStrings [" "] ["\\ "] app}
+    '')
+    cfg.hotApps);
 in
 
 {
   options.programs.skhd = {
-    extraConfig = lib.mkOption {
-      type = lib.types.lines;
-      default = "";
-      description = "Extra skhd configuration lines";
+    hotApps = lib.mkOption {
+      type = lib.types.addCheck (lib.types.listOf lib.types.str) (val: builtins.length val <= 7);
+      default = [ "Alacritty" "Firefox" "Mail" ];
+      description = ''
+        Up to seven apps to be assigned to hotkeys in the form of:
+        cmd + number (index of the app in the list, starting from 2)
+
+        cmd + 1 will automatically be assigned to Finder.
+      '';
     };
   };
-
 
   config = {
     home.packages = with pkgs; [
@@ -25,7 +35,10 @@ in
         # if there's already an instance running, open new window, otherwise start a new instance
         ctrl + cmd - return : ${config.programs.alacritty.package}/bin/alacritty msg create-window 2>&1 || /usr/bin/open -a ${config.programs.alacritty.package}/Applications/Alacritty.app
 
-        ${cfg.extraConfig}
+        # Application shortcuts
+        cmd + 1 : open -a Finder
+
+        ${hotApps}
       '';
 
       onChange = ''
