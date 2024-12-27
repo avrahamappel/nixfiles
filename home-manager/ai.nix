d1983f03a252cb7e4c909120b08cf795864bbc20
Avraham Appel <aappel248@gmail.com>
2024-12-27 10:09:28 -0500

    Copilot disabled by default

--- home-manager/ai.nix
+++ home-manager/ai.nix
@@ -3,7 +3,14 @@
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      copilot-vim
+      {
+        plugin = copilot-vim;
+        config = /* vim */ ''
+          "start with copilot disabled, with a map to reenable
+          Copilot disable
+          map <leader>i :Copilot enable<CR>
+        '';
+      }
     ];
   };
 }
