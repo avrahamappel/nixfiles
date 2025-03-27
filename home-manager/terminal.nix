58dd3698ac1730eb604dc703016649858bab1bb4
Avraham Appel <aappel248@gmail.com>
2025-03-27 17:43:34 -0400

    Fix $SHELL woes in tmux
    Apparently this has been an issue for some time, but I just noticed this
    week that tmux defaults to bash.
    
    I'm not sure if this is the same issue as
    [this](https://github.com/nix-community/home-manager/issues/5952), but
    trying the fix from
    [here](https://github.com/tmux/tmux/issues/4162#issuecomment-2412201969)

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -41,6 +41,10 @@
       # Undercurl settings
       set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
       set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+
+      # Fix $SHELL woes
+      set -gu default-command
+      set -g default-shell "$SHELL"
     '';
   };
 }
