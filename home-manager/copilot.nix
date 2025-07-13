13cc308edb6ccb7812c5ab60e7f4c70fca0c1a6c
Avraham Appel <aappel248@gmail.com>
2025-07-13 17:51:32 -0400

    Switch to codecompanion

--- home-manager/copilot.nix
+++ home-manager/copilot.nix
@@ -12,43 +12,19 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      lynx # copilot chat wants this
-    ];
-
-    # Shell aliases for github-copilot-cli
-    programs.zsh.initContent = ''
-      eval "$(${pkgs.github-copilot-cli}/bin/github-copilot-cli alias -- "$0")"
-    '';
-
-    programs.neovim.extraLuaPackages = ps: with ps; [
-      tiktoken_core # Optional dependency of CopilotChat-nvim
-    ];
-
     programs.neovim.plugins = with pkgs.vimPlugins; [
+      codecompanion-history-nvim
       {
-        plugin = copilot-lua;
+        plugin = codecompanion-nvim;
         type = "lua";
         config = /* lua */ ''
-          require('copilot').setup {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-            copilot_node_command = '${pkgs.nodejs}/bin/node',
-          }
-        '';
-      }
-      {
-        plugin = copilot-cmp;
-        type = "lua";
-        config = /* lua */ ''
-          require('copilot_cmp').setup {}
-        '';
-      }
-      {
-        plugin = CopilotChat-nvim;
-        type = "lua";
-        config = /* lua */ ''
-          require('CopilotChat').setup {}
+          require("codecompanion").setup({
+            extensions = {
+              history = {
+                enable = true
+              }
+            }
+          })
         '';
       }
     ];
