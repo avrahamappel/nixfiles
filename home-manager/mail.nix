03be20fda135b4cb10f44f040cc01707982e95f1
Avraham Appel <aappel248@gmail.com>
2025-08-10 20:18:51 -0400

    Add auto-light-dark ms theme

--- home-manager/mail.nix
+++ home-manager/mail.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }:
 
+let
+  npins = import ../npins;
+in
+
 {
   home.packages = [ pkgs.mailspring ];
+
+  home.file.".config/Mailspring/packages".source = pkgs.linkFarm "packages" {
+    "Mailspring Automatic Light-Dark Mode" =
+      npins."andrewminion/mailspring-automatic-light-dark-mode";
+  };
 }
