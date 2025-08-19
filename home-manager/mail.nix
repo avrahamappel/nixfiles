afe7ba041b83c4642c04d3492da124f09a2d4257
Avraham Appel <aappel248@gmail.com>
2025-08-18 23:47:25 -0400

    Change mailspring config location on darwin

--- home-manager/mail.nix
+++ home-manager/mail.nix
@@ -1,14 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 let
   npins = import ../npins;
+
+  mailspringPackages = {
+    source = pkgs.linkFarm "mailspring-packages" {
+      "Mailspring Automatic Light-Dark Mode" =
+        npins."andrewminion/mailspring-automatic-light-dark-mode";
+    };
+  };
 in
 
 {
   home.packages = [ pkgs.mailspring ];
 
-  home.file.".config/Mailspring/packages".source = pkgs.linkFarm "packages" {
-    "Mailspring Automatic Light-Dark Mode" =
-      npins."andrewminion/mailspring-automatic-light-dark-mode";
-  };
+  home.file.".config/Mailspring/packages" =
+    lib.mkIf pkgs.stdenv.isLinux mailspringPackages;
+
+  home.file."Library/Application Support/Mailspring/packages" =
+    lib.mkIf pkgs.stdenv.isDarwin mailspringPackages;
 }
