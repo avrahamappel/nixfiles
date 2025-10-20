f3ac73e3cb428665eceddc87883652623fd68f74
Avraham Appel <aappel248@gmail.com>
2025-10-19 23:03:50 -0400

    Put canon drivers behind option

--- nixos/printers.nix
+++ nixos/printers.nix
@@ -2,11 +2,13 @@
 
 {
   options = {
-    brother.enable = lib.mkEnableOption "Brother MFC-7360N printer support";
+    brother.enable = lib.mkEnableOption "Brother printers support";
+    canon.enable = lib.mkEnableOption "Canon printer support";
   };
 
   config = {
-    services.printing.drivers = [ pkgs.cnijfilter2 ]
+    services.printing.drivers =
+      lib.optional config.canon.enable pkgs.cnijfilter2
       ++ lib.optional config.brother.enable pkgs.brlaser;
 
     services.avahi = lib.mkIf config.brother.enable {
