efa960862bedb5b20eebb5cb9160aab5625e4d8c
Avraham Appel <aappel248@gmail.com>
2025-10-20 17:25:17 -0400

    Add HP printer driver

--- nixos/printers.nix
+++ nixos/printers.nix
@@ -4,12 +4,14 @@
   options = {
     brother.enable = lib.mkEnableOption "Brother printers support";
     canon.enable = lib.mkEnableOption "Canon printer support";
+    hp.enable = lib.mkEnableOption "HP printer support";
   };
 
   config = {
     services.printing.drivers =
       lib.optional config.canon.enable pkgs.cnijfilter2
-      ++ lib.optional config.brother.enable pkgs.brlaser;
+      ++ lib.optional config.brother.enable pkgs.brlaser
+      ++ lib.optional config.hp.enable pkgs.hplip;
 
     services.avahi = lib.mkIf config.brother.enable {
       enable = true;
