2ca16eb3a8be7ff177c3f5a62628e2c7260227f9
Avraham Appel <aappel248@gmail.com>
2025-12-01 23:42:00 -0500

    Update to NixOS 25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Entrypoint for both NixOS and standalone home-manager";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     home-manager = {
-      url = "home-manager/release-25.05";
+      url = "home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
