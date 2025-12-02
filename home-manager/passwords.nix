2fcb6aac081311850a6a501cfa25c05067ce1b9e
Avraham Appel <aappel248@gmail.com>
2025-12-01 23:11:33 -0500

    passwords: explicitly declare PASSWORD_STORE_DIR

--- home-manager/passwords.nix
+++ home-manager/passwords.nix
@@ -6,6 +6,7 @@
 
     password-store = {
       enable = true;
+      settings.PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
       package = pkgs.pass.withExtensions (exts: [
         exts.pass-audit
         exts.pass-update
