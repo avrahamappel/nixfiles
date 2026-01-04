d1edf8cdad8810ca06a58b8ea9eb2c108eeec1ed
Avraham Appel <aappel248@gmail.com>
2026-01-04 11:55:01 -0500

    gnome: add media hotkeys to config

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -39,79 +39,97 @@
   ];
 
   # My specific user account settings
-  home-manager.users.avraham = { lib, ... }: {
-    dconf.settings = with lib.hm.gvariant; {
-      # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
-      "org/gnome/desktop/interface" = {
-        clock-format = "12h";
-        clock-show-weekday = true;
-        color-scheme = "prefer-dark";
-        show-battery-percentage = true;
+  home-manager.users.avraham = { lib, config, ... }: {
+    options = {
+      hotkeys = {
+        media = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Whether to enable media control hotkeys. Set to false if your keyboard has dedicated media control keys.";
+        };
       };
+    };
 
-      "org/gnome/desktop/input-settings" = {
-        sources = [ (mkTuple [ "xkb" "us" ]) (mkTuple [ "xkb" "il" ]) ];
-      };
+    config = {
+      dconf.settings = with lib.hm.gvariant; {
+        # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+        "org/gnome/desktop/interface" = {
+          clock-format = "12h";
+          clock-show-weekday = true;
+          color-scheme = "prefer-dark";
+          show-battery-percentage = true;
+        };
 
-      "org/gnome/desktop/notifications" = {
-        show-in-lock-screen = false;
-      };
+        "org/gnome/desktop/input-settings" = {
+          sources = [ (mkTuple [ "xkb" "us" ]) (mkTuple [ "xkb" "il" ]) ];
+        };
 
-      "org/gnome/desktop/background" = {
-        color-shading-type = "solid";
-        picture-options = "zoom";
-        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-l.png";
-        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-d.png";
-        primary-color = "#77767B";
-        secondary-color = "#000000";
-      };
+        "org/gnome/desktop/notifications" = {
+          show-in-lock-screen = false;
+        };
 
-      "org/gnome/desktop/screensaver" = {
-        color-shading-type = "solid";
-        picture-options = "zoom";
-        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-l.png";
-        primary-color = "#77767B";
-        secondary-color = "#000000";
-      };
+        "org/gnome/desktop/background" = {
+          color-shading-type = "solid";
+          picture-options = "zoom";
+          picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-l.png";
+          picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-d.png";
+          primary-color = "#77767B";
+          secondary-color = "#000000";
+        };
 
-      "org/gnome/mutter" = {
-        dynamic-workspaces = true;
-      };
+        "org/gnome/desktop/screensaver" = {
+          color-shading-type = "solid";
+          picture-options = "zoom";
+          picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/vnc-l.png";
+          primary-color = "#77767B";
+          secondary-color = "#000000";
+        };
 
-      "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
+        "org/gnome/mutter" = {
+          dynamic-workspaces = true;
+        };
 
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        binding = "<Super>Return";
-        command = "alacritty";
-        name = "Open terminal";
-      };
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          ];
+        } // lib.optionalAttrs config.hotkeys.media {
+          next = [ "F10" ];
+          play = [ "F9" ];
+          previous = [ "F8" ];
+        };
 
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = [
-          "pip-on-top@rafostar.github.com"
-          "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-          "Vitals@CoreCoding.com"
-          "nightthemeswitcher@romainvigier.fr"
-          "appindicatorsupport@rgcjonas.gmail.com"
-          "bangs-search@suvan"
-        ];
-        favorite-apps = [
-          "firefox.desktop"
-          "Alacritty.desktop"
-          "org.gnome.Nautilus.desktop"
-          "Mailspring.desktop"
-        ];
-      };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Super>Return";
+          command = "alacritty";
+          name = "Open terminal";
+        };
 
-      "org/gnome/shell/extensions/pip-on-top" = {
-        stick = true;
-      };
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = [
+            "pip-on-top@rafostar.github.com"
+            "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+            "Vitals@CoreCoding.com"
+            "nightthemeswitcher@romainvigier.fr"
+            "appindicatorsupport@rgcjonas.gmail.com"
+            "bangs-search@suvan"
+          ];
+          favorite-apps = [
+            "firefox.desktop"
+            "Alacritty.desktop"
+            "org.gnome.Nautilus.desktop"
+            "Mailspring.desktop"
+          ];
+        };
 
-      "org/gnome/shell/extensions/vitals" = {
-        hot-sensors = [ "_memory_usage_" "_processor_usage_" "_storage_used_" ];
+        "org/gnome/shell/extensions/pip-on-top" = {
+          stick = true;
+        };
+
+        "org/gnome/shell/extensions/vitals" = {
+          hot-sensors = [ "_memory_usage_" "_processor_usage_" "_storage_used_" ];
+        };
       };
     };
   };
