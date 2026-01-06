803a82244ab8dace925f82c8351bf0f459a917d5
Avraham Appel <aappel248@gmail.com>
2026-01-06 01:09:28 -0500

    unfree: fix syntax

--- common/unfree.nix
+++ common/unfree.nix
@@ -2,11 +2,12 @@
 
 {
   options.unfreePackages = lib.mkOption {
-    type = with lib.types; listOf string;
+    type = with lib.types; listOf str;
+    default = [ ];
     description = "Which unfree packages to allow in this configuration";
   };
 
-  config = lib.mkIf config.unfreePackages != [ ] {
+  config = lib.mkIf (config.unfreePackages != [ ]) {
     nixpkgs.config.allowUnfreePredicate =
       pkg: builtins.elem (lib.getName pkg) config.unfreePackages;
   };
