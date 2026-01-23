2e9b4dfc5115bd42f2f3fd3114a04ef6a969d7e8
Avraham Appel <aappel248@gmail.com>
2026-01-22 21:01:51 -0500

    firefox: declare addon permissions

--- home-manager/firefox/default.nix
+++ home-manager/firefox/default.nix
@@ -10,20 +10,109 @@ in
   options.firefoxProfileDefaults = lib.mkOption {
     description = "Defaults for Firefox profiles, extracted to allow merging";
     default = {
-      extensions.packages = with rycee-nur.firefox-addons; [
-        adblocker-ultimate
-        browserpass
-        duckduckgo-privacy-essentials
-        surfingkeys
-        qr-code-address-bar
-      ] ++ (with local-addons; [
-        amazon-orders-to-beancount
-        cors-everywhere
-        # For downloading stuff from archive.org.
-        # Requires dsabling CORS to use, hence 
-        # the previous addon
-        internet_archive_downloader
-      ]);
+      extensions = with rycee-nur.firefox-addons; {
+        packages = [
+          adblocker-ultimate
+          browserpass
+          duckduckgo-privacy-essentials
+          surfingkeys
+          qr-code-address-bar
+        ] ++ (with local-addons; [
+          amazon-orders-to-beancount
+          cors-everywhere
+          # For downloading stuff from archive.org.
+          # Requires disabling CORS to use,
+          # hence the previous addon
+          internet_archive_downloader
+        ]);
+
+        exhaustivePermissions = true;
+        exactPermissions = true;
+        force = true;
+
+        settings = {
+          ${adblocker-ultimate.addonId}.permissions = [
+            "<all_urls>"
+            "contextMenus"
+            "http://*/*"
+            "https://*/*"
+            "storage"
+            "tabs"
+            "unlimitedStorage"
+            "webNavigation"
+            "webRequest"
+            "webRequestBlocking"
+          ];
+          ${browserpass.addonId}.permissions = [
+            "activeTab"
+            "alarms"
+            "clipboardRead"
+            "clipboardWrite"
+            "nativeMessaging"
+            "notifications"
+            "scripting"
+            "storage"
+            "tabs"
+            "webRequest"
+            "webRequestAuthProvider"
+          ];
+          ${duckduckgo-privacy-essentials.addonId}.permissions = [
+            "*://*/*"
+            "activeTab"
+            "alarms"
+            "<all_urls>"
+            "contextMenus"
+            "storage"
+            "tabs"
+            "webNavigation"
+            "webRequest"
+            "webRequestBlocking"
+          ];
+          ${surfingkeys.addonId}.permissions = [
+            "<all_urls>"
+            "bookmarks"
+            "clipboardRead"
+            "clipboardWrite"
+            "contextualIdentities"
+            "cookies"
+            "downloads"
+            "history"
+            "nativeMessaging"
+            "scripting"
+            "sessions"
+            "storage"
+            "tabs"
+            "topSites"
+          ];
+          ${qr-code-address-bar.addonId}.permissions = [ "activeTab" "menus" ];
+          ${local-addons.amazon-orders-to-beancount.addonId}.permissions = [
+            "*://*.amazon.ca/gp/*/order-history?*"
+            "*://amazon.ca/gp/*/order-history?*"
+            "*://*.amazon.ca/your-orders/orders?*"
+            "*://amazon.ca/your-orders/orders?*"
+            "*://*.amazon.com/gp/*/order-history?*"
+            "*://amazon.com/gp/*/order-history?*"
+            "*://*.amazon.com/your-orders/orders?*"
+            "*://amazon.com/your-orders/orders?*"
+            "storage"
+            "webRequest"
+          ];
+          ${local-addons.cors-everywhere.addonId}.permissions = [
+            "<all_urls>"
+            "storage"
+            "webRequest"
+            "webRequestBlocking"
+          ];
+          ${local-addons.internet_archive_downloader.addonId}.permissions = [
+            "declarativeNetRequest"
+            "downloads"
+            "notifications"
+            "scripting"
+            "storage"
+            "tabs"
+          ];
+        };
+      };
 
       search.default = "ddg";
       search.force = true; # Rebuilding fails without this
