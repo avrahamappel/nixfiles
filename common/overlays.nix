b12745cdeb89b979e5d2f68f3f09d7e9b12ac18c
Avraham Appel <aappel248@gmail.com>
2026-01-26 10:54:12 -0500

    overlays: remove setproctitle fix

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,27 +1,5 @@
-{ lib, ... }:
-
 {
   nixpkgs.overlays = [
-    # Fix setproctitle tests on macOS
-    # https://nixpk.gs/pr-tracker.html?pr=481408
-    (self: super:
-      lib.optionalAttrs super.stdenv.isDarwin {
-        python313 =
-          let
-            packageOverrides = python-self: python-super: {
-              setproctitle = python-super.setproctitle.overridePythonAttrs (prev:
-                assert builtins.length prev.disabledTests == 1;
-                {
-                  disabledTests = prev.disabledTests ++ [
-                    "test_fork_segfault"
-                    "test_thread_fork_segfault"
-                  ];
-                }
-              );
-            };
-          in
-          super.python313.override { inherit packageOverrides; };
-      })
     # More overlays here
   ];
 }
