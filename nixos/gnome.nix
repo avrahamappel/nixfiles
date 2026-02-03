7e4e3a74d43a815bd9d26948a2dd401361e06a07
Avraham Appel <aappel248@gmail.com>
2026-02-02 23:24:50 -0500

    gnome: add comment for appindicator

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -7,7 +7,7 @@
 
   # Gnome extensions
   environment.systemPackages = with pkgs; [
-    gnomeExtensions.appindicator
+    gnomeExtensions.appindicator # Necessary for mailspring icon
     gnomeExtensions.bangs-search # DuckDuckGo bangs in GNOME search
     gnomeExtensions.pip-on-top # Make picture-in-picture stay on top of all windows
     gnomeExtensions.vitals
