a9423b014c4337ee16f3a1e9b3ce13e4906b7ea6
Avraham Appel <aappel248@gmail.com>
2026-02-15 21:00:56 -0500

    hledger: add conf file and refactor

--- home-manager/hledger.nix
+++ home-manager/hledger.nix
@@ -1,39 +1,60 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, options, ... }:
 
 let
-  cfg = config.programs.hledger;
+  cfg = config.hledger;
+
+  hledgerPath = config.xdg.dataHome + "/hledger";
+
+  settingsPath = "hledger/hledger.conf";
+  fullSettingsPath = "${config.xdg.configHome}/${settingsPath}";
 in
 
 {
-  options.programs.hledger = {
+  options.hledger = with lib.types; {
     enable = lib.mkEnableOption "Enable hledger";
 
-    hledgerPath = lib.mkOption {
-      description = "Path to hledger files";
-      default = config.xdg.dataHome + "/hledger";
+    settings = lib.mkOption {
+      type = lines;
+      description = "Content of hledger.conf file";
     };
   };
 
   config = lib.mkIf cfg.enable {
+    hledger.settings = ''
+      --pretty
+      --tree
+    '';
+
     home.packages = [ pkgs.hledger ];
 
-    home.sessionVariables.LEDGER_FILE = cfg.hledgerPath +
-      "/ledgers/$(${pkgs.coreutils}/bin/date +%Y)/main.journal";
+    home.sessionVariables = {
+      HLEDGER_PATH = hledgerPath;
+      LEDGER_FILE = hledgerPath + "/ledgers/$(${pkgs.coreutils}/bin/date +%Y)/main.journal";
+    };
+
+    xdg.configFile.${settingsPath}.text = cfg.settings;
 
     home.shellAliases = {
-      hl = "hledger --pretty";
-      hlo = "cd ${cfg.hledgerPath} && e $LEDGER_FILE";
+      hl = "hledger --conf ${fullSettingsPath}";
+      hlo = "cd ${hledgerPath} && e $LEDGER_FILE";
       hlb = "hl bal --budget -V --invert 'expenses|income'";
-      hl24 = "hl -f ${cfg.hledgerPath}/ledgers/2024/main.journal";
-      hl25 = "hl -f ${cfg.hledgerPath}/ledgers/2025/main.journal";
+      hl24 = "hl -f ${hledgerPath}/ledgers/2024/main.journal";
+      hl25 = "hl -f ${hledgerPath}/ledgers/2025/main.journal";
     };
 
     services.git-sync.enable = true;
     services.git-sync.repositories.hledger = {
-      path = cfg.hledgerPath;
+      path = hledgerPath;
       uri = "git@github.com:avrahamappel/pta.git";
     };
 
     programs.neovim.plugins = [ pkgs.vimPlugins.vim-ledger ];
+
+    assertions = [
+      {
+        assertion = !(options.programs ? hledger) && !(options.services ? hledger);
+        message = "Upstream hledger module exists";
+      }
+    ];
   };
 }
