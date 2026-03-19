c792456efc20589181d3b4a1ed5ab571ad616707
Avraham Appel <aappel248@gmail.com>
2026-03-19 17:35:27 -0400

    overlays: add mpv cd support

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,5 +1,20 @@
+{ lib, config, ... }:
+
 {
-  nixpkgs.overlays = [
-    # More overlays here
-  ];
+  options = {
+    mpvCdSupport = lib.mkEnableOption "Whether to enable MPV to read directly from CD drives, i.e. `mpv cdda://`";
+  };
+
+  config = {
+    nixpkgs.overlays = [
+      # More overlays here
+    ]
+    ++ lib.optional config.mpvCdSupport (self: super: {
+      mpv = super.mpv.override {
+        mpv = super.mpv-unwrapped.override {
+          cddaSupport = true;
+        };
+      };
+    });
+  };
 }
