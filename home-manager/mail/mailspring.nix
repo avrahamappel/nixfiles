2c231611813f777b201ca71110377c32c12bd9f6
Avraham Appel <aappel248@gmail.com>
2026-07-22 00:39:26 -0400

    mailspring: 1.22.0 -> 1.23.0

--- home-manager/mail/mailspring.nix
+++ home-manager/mail/mailspring.nix
@@ -8,8 +8,12 @@
 let
   # Not npinning as the nixpkgs version downloads the prebuilt package
   # and I don't want to roll my own build
-  version = "1.22.0";
-  hash = "sha256-428i++JBArM8kcU7PFxSTUF7jTnq8r4VXNEuSUVqXeA=";
+  version = "1.23.0";
+  # NOTE: this is the hash for x86_64-linux. You can get it by visiting
+  # the release in Github, clicking the copy icon next to the amd64.deb
+  # asset, and running:
+  # wl-paste | sed 's/sha256://' | xxd -r -p | base64 | read hash && echo "sha256-$hash" | tee /dev/tty | wl-copy
+  hash = "sha256-9vYpN1IwBXsbYSOlxVsA4fftYGsEB5purqddaBkz5ck=";
 
   assertions = [
     {
@@ -46,6 +50,8 @@ let
         ${lib.getExe patchelf} --add-needed libEGL.so.1 \
           $out/share/mailspring/libEGL.so
       '';
+
+    meta.platforms = [ "x86_64-linux" ];
   });
 in
 
