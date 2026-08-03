6191e25cfb1e74de1a69029860a19bc7ceac3069
Avraham Appel <aappel248@gmail.com>
2026-08-03 01:22:36 -0400

    Use a maintained fork of intel-vaapi-driver

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,5 +1,9 @@
 { lib, config, ... }:
 
+let
+  inherit (import ../npins) irql-vaapi-driver;
+in
+
 {
   options = {
     mpvCdSupport = lib.mkEnableOption "Whether to enable MPV to read directly from CD drives, i.e. `mpv cdda://`";
@@ -7,7 +11,13 @@
 
   config = {
     nixpkgs.overlays = [
-      # More overlays here
+      # Use a maintained fork of intel-vaapi-driver
+      (final: prev: {
+        intel-vaapi-driver = prev.intel-vaapi-driver.overrideAttrs {
+          version = irql-vaapi-driver.version;
+          src = irql-vaapi-driver;
+        };
+      })
     ]
     ++ lib.optional config.mpvCdSupport (self: super: {
       mpv-unwrapped = super.mpv-unwrapped.override {
