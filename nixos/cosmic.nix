243ef80eb483b15952dc595e7e2e179b6f269385
Avraham Appel <aappel248@gmail.com>
2026-08-19 08:41:04 -0400

    cosmic: use "out" output for gstreamer

--- nixos/cosmic.nix
+++ nixos/cosmic.nix
@@ -17,7 +17,7 @@ in
     # Set GStreamer variable so cosmic-player works
     environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
       lib.makeSearchPath "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
-        gstreamer
+        gstreamer.out
         gst-plugins-base
         gst-plugins-good
       ]);
